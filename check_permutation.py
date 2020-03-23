f8c716ed8e87fa271d04082030ef2d1556d27324
yju0044 <55807908+y0214ju@users.noreply.github.com>
2020-03-23 14:11:51 -0500

    Update check_permutation.py

--- check_permutation.py
+++ check_permutation.py
@@ -1,12 +1,30 @@
+#solve 1: sort strings and compare
+# def check_permutation(a: str, b: str): 
+#     if len(a) != len(b):
+#         return False
+#     str1 = "".join(sorted(a))
+#     str2 = "".join(sorted(b))
+#     for i in range(len(str1)):
+#         if str1[i] != str2[i]:
+#             return False
+#     return True
+
+#solve 2: using charcter counter"
 def check_permutation(a: str, b: str): 
-    if len(a) != len(b):
-        return False
-    str1 = "".join(sorted(a))
-    str2 = "".join(sorted(b))
-    for i in range(len(str1)):
-        if str1[i] != str2[i]:
-            return False
-    return True
+  if len(a) != len(b):
+    return False
+  
+  letters = [0] * 128
+  for i in range(len(a)):
+    letters[ord(a[i])] += 1
+
+  for i in range(len(a)):
+    letters[ord(b[i])] -= 1
+    if letters[ord(b[i])] < 0:
+      return False
+
+  return True     
+
 
 if __name__ == "__main__":
     if check_permutation("Test", "test"):
